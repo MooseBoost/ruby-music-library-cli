3d5c9a568162a8fd384e73a0e58d103c53d4c3d1
Nathaniel Miller <dukeoflaser@gmail.com>
2016-03-05 22:05:52 +0000

    Add #find_by_name and #find_or_create_by_name to Concerns::Findable

--- concerns/findable.rb
+++ concerns/findable.rb
@@ -1,2 +1,14 @@
 module Concerns::Findable
+  
+  def find_by_name(name)
+    all.find { |instance| instance.name == name }    
+  end
+    
+  def find_or_create_by_name(name)
+    instance = find_by_name(name)
+    instance = create(name) if song.nil?
+      
+    instance
+  end
+  
 end
